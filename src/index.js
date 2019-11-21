65304563d83cf7e1943a9bb3699661057410868a
shubh2912 <arora.shubham63@gmail.com>
2019-11-21 23:32:18 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux';
+import store from './store';
+import MoviesList from './container/movies_list';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <MoviesList />
+    </Provider>, document.getElementById('root'));
+
